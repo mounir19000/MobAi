cfbd8a3612016d44e245e1a056b23ad47dbe4ed3
m3kido <63561969+m3kido@users.noreply.github.com>
2025-02-13 22:59:27 +0100

    firebase connection

--- FrontEnd/app/lib/main.dart
+++ FrontEnd/app/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -31,4 +32,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
